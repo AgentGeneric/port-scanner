4a21cd22278b439fffaaca8723c4ea1d5a58370f
c00LAgent <70501704+c00LAgent@users.noreply.github.com>
2021-02-26 02:01:20 -0500

    Add files via upload

---
+++ port-scanner.py
@@ -0,0 +1,138 @@
+import socket
+import time
+import os
+
+
+print("""
+
+██████╗  ██████╗ ██████╗ ████████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ 
+██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗
+██████╔╝██║   ██║██████╔╝   ██║       ███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
+██╔═══╝ ██║   ██║██╔══██╗   ██║       ╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
+██║     ╚██████╔╝██║  ██║   ██║       ███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║
+╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
+                                                                                                  
+""")
+
+#Spaces
+print("")
+
+#IPV4 and Handshake Method
+
+# Handshake Method Menu
+print("[*] Select scan type from menu")
+
+#Spaces
+print("")
+print("")
+
+print("0) TCP Scan")
+print("1) Send Ping")
+
+#Spaces
+print("")
+print("")
+
+print("99) Exit")
+
+#Spaces
+print("")
+print("")
+
+ScanMethod = int(input("scan> "))
+
+#Initiate Scan
+# Enum and Scan
+
+def scanPort(port):
+    if s.connect_ex((inputIpHost, port)):
+        print("[*] port", port, "is closed")
+    else:
+        print("[*] port", port, "is open")
+
+
+
+#ScanType
+if ScanMethod == 0:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Ip and Port
+    inputIpHost = input("[+] Enter host ip: ")
+    port = int(input("[+] Specify Port:"))
+
+
+    # Spaces
+    print("")
+    print("")
+
+    print("[*] Scanning...")
+
+    # Spaces
+    print("")
+    print("")
+
+    scanPort(port)
+
+elif ScanMethod == 1:
+
+    # Ip and Port
+    inputIpHost = input("[+] Enter host ip: ")
+    ip = socket.gethostbyname(inputIpHost)
+
+    # Spaces
+    print("")
+    print("")
+
+    print("[*] Pinging...")
+
+    pingResponse = os.system("ping " + inputIpHost)
+
+    if pingResponse == 0:
+        #Spaces
+        print("")
+        print("")
+
+        print("[*]",inputIpHost,"(",ip,") is up")
+
+    else:
+        print("[*]", inputIpHost, (ip), "is down")
+
+elif ScanMethod == 99:
+    #Spaces
+    print("")
+    print("")
+
+    #Exit
+    print("[+] Exiting...")
+    time.sleep(0.5)
+    exit()
+
+else:
+    # Spaces
+    print("")
+    print("")
+
+    print("[+] Please select from Menu")
+    time.sleep(0.5)
+
+    #Spaces
+    print("")
+    print("")
+
+    #Exit
+    print("[+] Exiting...")
+    time.sleep(0.5)
+    exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
